c77ecd4ab765a3e402b6663a3b726e35518f7b8f
JuanMartinMena <juanmartinmenac@gmail.com>
2024-08-22 18:29:24 +0000

    env solution?

--- Shellmania.c
+++ Shellmania.c
@@ -25,7 +25,9 @@ void free_array(char **array)
  *
  * Return: return 0 if end
  */
-int main(int ac, char **av, char **env)
+int main(__attribute__((unused))int ac,
+	 __attribute__((unused))char **av,
+	 char **env)
 {
 	char *path, *line = NULL, *s = "sh", *a;
 	size_t len = 0;
@@ -34,8 +36,6 @@ int main(int ac, char **av, char **env)
 	int counter = 0;
 	char **xd = NULL;
 
-	(void)ac;
-	(void)av;
 	while (1)
 	{
 		counter++;
@@ -48,26 +48,22 @@ int main(int ac, char **av, char **env)
 		if (xd == NULL)
 			continue;
 		if (strcmp(xd[0], "exit") == 0)
-		{
-			free_array(xd);
-			free(line);
-			exit(EXIT_SUCCESS);
-		}
+			free_array(xd), free(line), exit(EXIT_SUCCESS);
 		if (strcmp(xd[0], "env") == 0)
 			print_array(env);
-		if (stat(xd[0], &sb) != -1)
-			f_w_e(xd[0], xd, NULL);
 		else
 		{
-			path = Recorrer_el_path(xd[0]);
-			if (path == NULL)
-			{
-				a = _perror(s, counter, line);
-				free(a);
-			}
+			if (stat(xd[0], &sb) != -1)
+				f_w_e(xd[0], xd, NULL);
 			else
-				f_w_e(path, xd, NULL);
-		free(path);
+			{
+				path = Recorrer_el_path(xd[0]);
+				if (path == NULL)
+					a = _perror(s, counter, line), free(a);
+				else
+					f_w_e(path, xd, NULL), free(path);
+			}
+
 		}
 		free_array(xd);
 	}
@@ -89,7 +85,7 @@ char *_perror(char *s, int counter, char *l)
 
 	l[strcspn(l, "\n")] = 0;
 	a = malloc(sizeof(char *) * strlen(s) + strlen(l) + sizeof(int) + 5);
-	sprintf(a,"%s: %i: %s ", s, counter, l);
+	sprintf(a, "%s: %i: %s ", s, counter, l);
 	perror(a);
 	return (a);
 }
@@ -102,6 +98,6 @@ char **print_array(char **array)
 
 		printf("%s\n", array[i]);
 		i++;
-	}	
+	}
 	return (array);
 }
